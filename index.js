e0cf4f69c2fa114eb5c9113a59f3879d9c88479f
Tasokvaratskhel1a <tasokvaratskhelia19@gmail.com>
2025-03-27 18:14:42 +0400

    დავამატე index.js და index.html

---
+++ index.js
@@ -0,0 +1,50 @@
+document.getElementById("loadPosts").addEventListener("click", function() {
+    const postsData = [
+        {
+            "userId": 1,
+            "id": 1,
+            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum..."
+        },
+        {
+            "userId": 1,
+            "id": 2,
+            "title": "qui est esse",
+            "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores..."
+        },
+        {
+            "userId": 1,
+            "id": 3,
+            "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+            "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad..."
+        },
+        {
+            "userId": 1,
+            "id": 4,
+            "title": "eum et est occaecati",
+            "body": "ullam et saepe reiciendis voluptatem adipisci..."
+        },
+        {
+            "userId": 1,
+            "id": 5,
+            "title": "nesciunt quas odio",
+            "body": "repudiandae veniam quaerat sunt sed..."
+        },
+        {
+            "userId": 1,
+            "id": 6,
+            "title": "dolorem eum magni eos aperiam quia",
+            "body": "ut aspernatur corporis harum nihil quis provident sequi..."
+        }
+    ];
+
+    const postsContainer = document.getElementById("posts");
+    postsContainer.innerHTML = ""; 
+
+    postsData.forEach(post => {
+        const postElement = document.createElement("div");
+        postElement.classList.add("post");
+        postElement.innerHTML = `<h2>${post.title}</h2><p>${post.body}</p>`;
+        postsContainer.appendChild(postElement);
+    });
+});
